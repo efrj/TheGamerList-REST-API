c6e28877256ad20c9b138d1761d52d54c638c268
Edivaldo Reis <efrj@hotmail.com>
2023-05-27 00:30:08 -0300

    Added genre rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require_relative 'support/factory_bot'
+#require_relative 'support/factory_bot'
 require_relative 'support/chrome'
+require 'factory_bot_rails'
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -63,4 +64,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include FactoryBot::Syntax::Methods
 end
